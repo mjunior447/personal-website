eec36b9c2e3ae6ecfb17d22029f744d010d86f6e
Mauricio Antero <mjunior447@gmail.com>
2025-03-04 12:21:01 -0300

    feat: project initial structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-300">
+      <h1>Mauricio Antero</h1>
+      <h2>Front End Engineer</h2>
+      <p>I build accessible, pixel-perfect digital experiences for the web.</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,15 @@
+const MainContent = () => {
+  return (
+    <main className="bg-red-300">
+      other details aobuds asodjap asdijasdjas doasd aios dhaiosdfhaiosdfh aosid
+      fhaosid fhaosidfh asoifhu aosidf hiaosd fhuaosidfh aosid fhuaosidf
+      haosidfhadosih aidos fdhuasifdud dhufoifhaoifdhsu oa
+      hoasdfhofhiaodhsfaoifhaoisfdhaosifdhaoifdhfdhiaoisfh
+      asdfhaoisfdhoasdhoiasfdhodsi hidshfas fdh dhoia fdhsoi fdhoi
+      afhdsofadhsfahdsoi fadhsiohfadshoiashoihdsfahfdshds hd hfdao dhfah dsfa
+      hsoadhsf
+    </main>
+  );
+};
+
+export default MainContent;
